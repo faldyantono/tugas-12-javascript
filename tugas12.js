db9647987c1ce625764b3fd8d4f4db3163ea9b2c
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-21 00:41:58 +0700

    Add files via upload

---
+++ tugas12.js
@@ -0,0 +1,9 @@
+function belanja() {
+  var buah = ["pisang", "jeruk", "apel", "mangga"]
+  console.log(buah);
+  buah.pop()
+  console.log(buah);
+  buah.shift()
+  return buah
+}
+console.log(belanja());
